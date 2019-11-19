b69bc8585061a8905770fd67907961ac48b86f47
watslink <seb.decroix@gmail.com>
2019-11-19 13:40:20 +0100

    remove outOfDate borrowing of listBorrowings

--- bibliowebclient-webapp/src/main/java/com/sd/oc/webapp/controller/BorrowingController.java
+++ bibliowebclient-webapp/src/main/java/com/sd/oc/webapp/controller/BorrowingController.java
@@ -1,6 +1,7 @@
 package com.sd.oc.webapp.controller;
 
 
+import com.sd.oc.serviceproxy.generated.borrowingAPI.Borrowing;
 import com.sd.oc.serviceproxy.generated.borrowingAPI.BorrowingServiceAPI;
 import com.sd.oc.serviceproxy.generated.userAPI.User;
 import com.sd.oc.serviceproxy.generated.userAPI.UserServiceAPI;
@@ -10,6 +11,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 public class BorrowingController {
@@ -23,7 +26,19 @@ public class BorrowingController {
     @GetMapping("/borrowings")
     public String getBorrowings(Model model){
         User user= userServiceAPI.findUserByUsername(SecurityContextHolder.getContext().getAuthentication().getName());
-        model.addAttribute("listBorrowings",borrowingServiceAPI.findAllBorrowingOfUser(user.getUserId()));
+        List<Borrowing> borroOfUser = borrowingServiceAPI.findAllBorrowingOfUser(user.getUserId());
+        List<Borrowing> borroOutOfDateOfUser = borrowingServiceAPI.findAllBorrowingOutOfTimeOfUser(user.getUserId());
+
+        for(Borrowing borrowing: borroOfUser){
+            for(Borrowing borroOutOfDate: borroOutOfDateOfUser){
+                if(borroOutOfDate == borrowing){
+                    borroOfUser.remove(borrowing);
+                }
+            }
+        }
+
+        model.addAttribute("listBorrowings", borroOfUser);
+        model.addAttribute("listBorrowingsOutOfDate", borroOutOfDateOfUser);
         return "borrowings";
     }
 
